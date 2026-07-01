393f967157365ef1cebb7d54b1c139bc208c9306
Luxx Cargo LLC <luxxcargoLLC@gmail.com>
2026-07-01 16:17:49 -0400

    Add odds API placeholder

---
+++ api/odds_api.py
@@ -0,0 +1,9 @@
+import requests
+
+def get_odds():
+    print("\nFetching today's betting odds...")
+
+    # Placeholder until we connect a real odds provider
+    print("Astros -165")
+    print("Twins +140")
+    print("Total: 8.5")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.helpers import banner
 from models.predictor import predict
 from api.sports_api import get_games
+from api.odds_api import get_odds
 banner()
 
 print("System Status: ONLINE")
@@ -9,3 +10,4 @@ print("Version 1.0")
 
 predict()
 get_games()
+get_odds()
